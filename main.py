4dba975b0ca37c7d6e2796e006ca943758630133
Navas Azeez <navasfiroz@gmail.com>
2020-05-08 18:39:56 +0400

    few changes

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 from flask import Flask, render_template
-from objects import Organization
+from flask_sqlalchemy import SQLAlchemy
 
-# app = Flask(__name__)
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///main.db'
+db = SQLAlchemy(app)
+
+class Organization(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150), nullable=False)
+
+    def __repr__(self):
+        return "Organization id :"+str(self.id)
+
+db.create_all()
+new_org = Organization(name="Tawook Nation")
+db.session.add(new_org)
+print(Organization.query.get(1).name)
+
 
 # @app.route('/')
 # def index_page():
@@ -9,6 +26,3 @@ from objects import Organization
 
 # app.run(debug=True)
 
-jp = Organization("JustProperty")
-
-print(jp)
